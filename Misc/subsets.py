2ea008418bfc0066cfac800a3423463163d92f58
ajita <ajit.arora@druva.com>
2017-03-07 17:23:07 +0530

    subsets.py

--- Misc/subsets.py
+++ Misc/subsets.py
@@ -10,4 +10,38 @@ def x(s):
    l*=2
   print ""
 
-x(3)
+# 'abc' > ["abc", "abC", "aBc", "Abc", "ABc", "AbC", "aBC", "ABC"]
+def func(str):
+ ret = []
+ j = pow(2, len(str))
+ for i in xrange(0, j):
+  l = 1
+  s = ""
+  for k in xrange(0, len(str)):
+   if i&l:
+    s += str[k].upper()
+   else:
+    s += str[k].lower()
+   l*=2
+  ret.append(s)
+ return ret
+
+def func1(str):
+ ret = set()
+ j = pow(2, len(str))
+ for i in xrange(0, j):
+  l = 1
+  s = ""
+  for k in xrange(0, len(str)):
+   if i&l:
+    s += str[k].upper()
+   else:
+    s += str[k].lower()
+   l*=2
+  ret.add(s)
+ return list(ret)
+
+# '0ab' > ["0ab", "0aB", "0Ab", "0AB"]
+
+print func1('abc')
+print func1('abc0')
